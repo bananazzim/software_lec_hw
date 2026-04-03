c6d7c614e4393a783f64da2f734ac7e6f4518b11
bananazzim <105703035+bananazzim@users.noreply.github.com>
2026-04-04 03:34:13 +0900

    Add files via upload

---
+++ hw08/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():  
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/strawberry")
+def strawberry():
+    return render_template("strawberry.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    #템플릿에 다 박았는데 왜 첫화면만 나올까?
+    #이제는 서버끼리의 url연결을 통해 이동하기때문이야
+    #인덱스 안에 url주소를 수정해줘야해
+    
