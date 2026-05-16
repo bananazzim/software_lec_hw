6cdc34a215230df52b707007055a6fa5be780439
bananazzim <105703035+bananazzim@users.noreply.github.com>
2026-05-17 05:27:35 +0900

    Add files via upload

---
+++ hw12/mysite/urls.py
@@ -0,0 +1,45 @@
+"""
+URL configuration for mysite project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+
+from board.views import (
+    HomeView,
+    PostListView,
+    PostDetailView,
+    AboutView,
+    ProjectsView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', HomeView.as_view(), name='home'),
+
+    path('posts/', PostListView.as_view(), name='post_list'),
+
+    path(
+        'posts/<int:pk>/',
+        PostDetailView.as_view(),
+        name='post_detail'
+    ),
+
+    path('about/', AboutView.as_view(), name='about'),
+
+    path('projects/', ProjectsView.as_view(), name='projects'),
+]
